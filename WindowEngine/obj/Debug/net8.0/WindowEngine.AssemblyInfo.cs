f7bac9c38b7610bc6bddb48ccf9945b10984351a
Yevhen Chernytskyi <eugene.chernitskiy@gmail.com>
2025-09-29 12:37:29 -0400

    deleted global.json

--- WindowEngine/obj/Debug/net8.0/WindowEngine.AssemblyInfo.cs
+++ WindowEngine/obj/Debug/net8.0/WindowEngine.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WindowEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd0c23be2537d239feae1dc19e46ff167d1fd794")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+97b0c19185658d429f24f715c2e5b1bde033b9ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("WindowEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WindowEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
